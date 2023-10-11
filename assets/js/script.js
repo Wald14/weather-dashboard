a149da7eeed429a0202b15862706bf169483a10d
Wald14 <lukewald@hotmail.com>
2023-10-11 16:05:33 -0500

    outlined a general idea of how I want the website broken down in a new file index2.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,20 +25,13 @@
 // "https://api.openweathermap.org/data/2.5/weather?id=524901&appid=b590e1e6cab1a4667e5be277471ec18c"
 
 
-// Kelvin to Fahrenheit
-function kelvinToFahrenheit(kelvin) {
-  let x = (kelvin - 273.15) * (9 / 5) + 32;
-  return x.toFixed(2);
-}
 
-
-
+getCityInfo("Minneapolis");
 
 // Get Weather API
 function getCityInfo(city) {
 
-  // fetch request gets a list of all avaliable through the OpenWeather API
-  let requestCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b590e1e6cab1a4667e5be277471ec18c`
+  let requestCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=b590e1e6cab1a4667e5be277471ec18c`
 
   fetch(requestCity)
     .then(function (response) {
@@ -52,70 +45,57 @@ function getCityInfo(city) {
         lat: data.coord.lat,
         lon: data.coord.lon,
         name: data.name,
-        temp: kelvinToFahrenheit(data.main.temp),
-        temp_max: kelvinToFahrenheit(data.main.temp_max),
-        temp_min: kelvinToFahrenheit(data.main.temp_min),
+        temp: (data.main.temp),
         humidity: data.main.humidity,
-        // weather: data.weather[0].main,
+        weather: data.weather[0].main,
         wind_speed: data.wind.speed
       }
-
+      getFiveDayForcast(cityInfo.lat, cityInfo.lon);
       console.log(cityInfo)
     })
 }
 
-getCityInfo("Minneapolis");
-// console.log(getCityInfo("Minnesapolis"))
 
-
-
-
-// Get Weather API
+// Get Five Day Forcast
 function getFiveDayForcast(lat, lon) {
 
   // fetch request gets a list of all avaliable through the OpenWeather API
-  let requestFiveDayForcast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=b590e1e6cab1a4667e5be277471ec18c`
+  let requestFiveDayForcast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=b590e1e6cab1a4667e5be277471ec18c`
 
   fetch(requestFiveDayForcast)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-
       console.log(data)
 
       let fiveDayForcast = [];
-
       // Gives 40 data points, 8 per day
           // Day 1: 0-7
-          // Set 1: 12am - 2am
-          // Set 2: 3am - 5am
-          // Set 3: 6am - 8am
-          // Set 4: 9am - 11am
-          // Set 5: 12pm - 2pm
-          // Set 6: 3pm - 5pm
-          // Set 7: 6pm - 8pm
-          // Set 8: 9pm - 11pm
+              // Set 1: 12am - 2am
+              // Set 2: 3am - 5am
+              // Set 3: 6am - 8am
+              // Set 4: 9am - 11am
+              // Set 5: 12pm - 2pm
+              // Set 6: 3pm - 5pm
+              // Set 7: 6pm - 8pm
+              // Set 8: 9pm - 11pm
           // Day 2: 8-15
           // Day 3: 16-23
           // Day 4: 24-31
           // Day 5: 32-39
-      // Currently just grabbing the days first data point
-      // Later grab all and determine the days true high and low
+      // Currently just grabbing the days 5th data point
       for (i = 5; i < 40; i += 8) {
         let singleDayForcast = {
           humidity: data.list[i].main.humidity,
           name: data.city.name,
-          temp_max: kelvinToFahrenheit(data.list[i].main.temp_max),
-          temp_min: kelvinToFahrenheit(data.list[i].main.temp_min),
+          temp_max: data.list[i].main.temp_max,
+          temp_min: data.list[i].main.temp_min,
           weather: data.list[i].weather[0].main,
           wind_speed: data.list[i].wind.speed
         }
         fiveDayForcast.push(singleDayForcast)
       }
-
       console.log(fiveDayForcast)
     })
 }
-
-getFiveDayForcast(44.98, -93.2638);
